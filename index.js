18fb1ce378456c81af0d9585497cd5fb5a70c120
dunglt94 <tuandung9594@gmail.com>
2024-09-12 16:59:11 +0700

    update cac chuc nang tinh

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-// phép cộng
+let first_ele = document.getElementById("first_no");
+let second_ele = document.getElementById("second_no");
+let showresult = document.getElementById("showresult")
+// Phép cộng
 function addition() {
-    let first_no = +document.getElementById("first_no").value;
-    let second_no = +document.getElementById("second_no").value;
-    let add = first_no + second_no;
-    document.getElementById("result").innerText = `Result: ${add}`;
+    let a = +first_ele.value;
+    let b = +second_ele.value;
+    let result = a + b;
+    showresult.innerText = `Result: ${result}`;
+}
+// Phép trừ
+function subtraction() {
+    let a = +first_ele.value;
+    let b = +second_ele.value;
+    let result = a - b;
+    showresult.innerText = `Result: ${result}`;
+}// Phép nhân
+function multiplication() {
+    let a = +first_ele.value;
+    let b = +second_ele.value;
+    let result = a * b;
+    showresult.innerText = `Result: ${result}`;
+}// Phép chia
+function division() {
+    let a = +first_ele.value;
+    let b = +second_ele.value;
+    let result = a / b;
+    showresult.innerText = `Result: ${result}`;
 }
